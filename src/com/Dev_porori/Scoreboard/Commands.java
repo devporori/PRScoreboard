86a68eb8ffc5b8edf187836a077a8ac8e076e868
devporori <hyogyu4981@naver.com>
2019-02-01 19:04:08 +0900

    1.0.3 update
    1. 펄미션 추가

--- src/com/Dev_porori/Scoreboard/Commands.java
+++ src/com/Dev_porori/Scoreboard/Commands.java
@@ -13,12 +13,20 @@ public class Commands implements CommandExecutor {
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 		if (label.equals("prboard")) {
 			if (args.length == 0) {
-				sendMessage(sender, "/prboard toggle");
-				if (sender.isOp())
-					sendMessage(sender, "/prboard reload");
+				if (!sender.hasPermission("PRScoreboard.help")) {
+					sendMessage(sender, "당신은 권한이 없습니다.");
+					return false;
+				}
+				sendMessage(sender, "/prboard toggle  §8|  §7스코어보드를 활성화 하거나 비활성화합니다.");
+				if (sender.hasPermission("PRScoreboard.reload"))
+					sendMessage(sender, "/prboard reload  §8|  §7콘피그 파일을 다시 불러옵니다.");
 				return false;
 			}
 			if (args[0].equals("toggle")) {
+				if (!sender.hasPermission("PRScoreboard.toggle")) {
+					sendMessage(sender, "당신은 권한이 없습니다.");
+					return false;
+				}
 				if (!(sender instanceof Player)) {
 					sendMessage(sender, "플레이어만 사용할수있는 명령어입니다.");
 					return false;
