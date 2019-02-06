d76bf6057e283dd90dfd77515286129c088346b1
devporori <hyogyu4981@naver.com>
2019-02-06 19:43:32 +0900

    fix
    Null이 뜨는 오류를 제거

--- src/com/Dev_porori/Scoreboard/PlayerListener.java
+++ src/com/Dev_porori/Scoreboard/PlayerListener.java
@@ -12,6 +12,7 @@ public class PlayerListener implements Listener {
 	@EventHandler
 	public void onJoin(PlayerJoinEvent e) {
 		us.put(e.getPlayer(), true);
+		tn.put(e.getPlayer(), 0);
 		setScoreboard(e.getPlayer());
 	}
 	
